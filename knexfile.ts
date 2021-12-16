010f892ea6a94a4c47d612e7737e888d5aee9695
Aleksa Crveni <sestudent321@gmail.com>
2021-12-16 15:58:01 +0100

    Knex

---
+++ knexfile.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+module.exports = {
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    port: Number(process.env.DB_PORT),
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  }
+}
