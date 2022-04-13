37e879109913a6ce9150e6ac8c2e5e84445c9dfe
WickedyWick <crvenialeksa@gmail.com>
2022-04-13 23:00:34 +0200

    validation and test

--- src/validators/homeValidator.ts
+++ src/validators/homeValidator.ts
@@ -26,6 +26,8 @@ export const gameCreateValidator = validator([
         .bail()
         .isLength({ min: 4, max: 30 })
         .bail()
+        .matches('^[A-Za-zа-ш0-9А-ШčČćĆžŽšŠđĐђјљњћџЂЈЉЊЋЏ ]{4,30}$','g')
+        .bail()
     ,
     check('roundTimeLimit')
         .escape()
@@ -35,7 +37,8 @@ export const gameCreateValidator = validator([
         .custom(( value, { req }) => {
             if (roundTimeLimit[value] === undefined) throw new Error('Incorrect round time limit value!')
             return true
-        }),
+        })
+        .bail(),
     check('playerCount')
         .escape()
         .bail()
@@ -47,4 +50,29 @@ export const gameCreateValidator = validator([
             if (playerCount[value] === undefined) throw new Error('Invalid number of players')
             return true
         })
+])
+
+export const regUserValidator = validator([
+    check('username')
+        .escape()
+        .bail()
+        .exists({ checkFalsy: true})
+        .bail()
+        .isString()
+        .bail()
+        .isLength({ min: 4, max: 30 })
+        .bail()
+        .matches('^[A-Za-zа-ш0-9А-ШčČćĆžŽšŠđĐђјљњћџЂЈЉЊЋЏ ]{4,30}$','g')
+        .bail(),
+    check('roomCode')
+        .escape()
+        .bail()
+        .exists({ checkFalsy: true })
+        .bail()
+        .isString()
+        .bail()
+        .isLength({ min: 8, max : 8 })
+        .bail()
+        .matches('^[A-Za-z0-9]{8}$','g')
+        .bail()
 ])
