23625218b936bbcc4a59c1beceff2c242931408e
WickedyWick <crvenialeksa@gmail.com>
2022-06-30 22:54:00 +0200

    Added round model

---
+++ src/database/models/round.ts
@@ -0,0 +1,35 @@
+import { Model, BaseModel, ModelDate } from 'database/model'
+
+export interface RoundFields {
+    id: number,
+    room_code: string,
+    letter: string,
+    round_number: number,
+    created_at: ModelDate,
+    updated_at: ModelDate
+}
+
+export interface RoundMethods { }
+
+export type RoundModel = Model<RoundFields, RoundMethods>
+
+export class Round extends BaseModel<RoundFields, RoundMethods> {
+    fields = [
+        'id',
+        'room_code',
+        'letter',
+        'round_number',
+        'created_at',
+        'updated_at'
+    ]
+
+    constructor () {
+        super('room')
+    }
+
+    instanceMethods: RoundMethods = {
+
+    }
+}
+
+export default new Round()
