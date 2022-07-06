a89b34e161af1280bee5c81e8ea9951a42e303dd
WickedyWick <crvenialeksa@gmail.com>
2022-07-06 17:08:56 +0200

    Added migration for results table

---
+++ migrations/20220706145553_add_results_table.ts
@@ -0,0 +1,25 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('results', (t) => {
+        t.increments('id')
+        t.integer('player_id').references('id').inTable('player').notNullable()
+        t.foreign('round_id').references('id').inTable('round').notNullable()
+        t.string('drzava')
+        t.string('grad')
+        t.string('ime')
+        t.string('biljka')
+        t.string('zivotinja')
+        t.string('planina')
+        t.string('reka')
+        t.string('predmet')
+        t.integer('poeni').defaultTo(0).notNullable()
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable('results')
+}
+
