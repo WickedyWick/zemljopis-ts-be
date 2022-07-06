dec3e3a1059db0468737305ab386a9aec383ad10
WickedyWick <crvenialeksa@gmail.com>
2022-07-06 18:33:49 +0200

    Result model addeed

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -3,9 +3,12 @@ import { Player as PlayerModel } from './player'
 import { FieldData as FieldDataModel } from './fieldData'
 import { Category as CategoryModel } from './category'
 import { Round as RoundModel } from './round'
+import { Result as ResultModel } from './result'
+
 
 export const Room = new RoomModel()
 export const Player = new PlayerModel()
 export const FieldData = new FieldDataModel()
 export const Category = new CategoryModel()
 export const Round = new RoundModel()
+export const Result = new ResultModel()
---
+++ src/database/models/result.ts
@@ -0,0 +1,47 @@
+import { BaseModel, Model } from "database/model";
+
+export interface ResultFields {
+    id: number
+    round_id: number
+    player_id: number
+    drzava: string
+    grad: string
+    ime: string
+    biljka: string
+    zivotinja: string
+    planina: string
+    reka: string
+    predmet: string
+    points: number
+}
+
+export interface ResultMethods { }
+
+export type ResultModel = Model<ResultFields, ResultMethods>
+
+export class Result extends BaseModel<ResultFields, ResultMethods> {
+    fields: [
+        'id',
+        'round_id',
+        'player_id',
+        'drzava',
+        'grad',
+        'ime',
+        'biljka',
+        'zivotinja',
+        'planina',
+        'reka',
+        'predmet',
+        'points',
+    ]
+
+    constructor () {
+        super('result')
+    }
+
+    instanceMethods: {
+
+    }
+}
+
+export default new Result()
