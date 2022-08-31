8bf3e090c5dc8a5cad05a416901a1e645d4e17b7
WickedyWick <crvenialeksa@gmail.com>
2022-08-31 19:09:11 +0200

    Controller handled

---
+++ src/controllers/pageControllers/suggestion.controller.ts
@@ -0,0 +1,20 @@
+import { logError } from 'utils/logger'
+import { Action } from 'utils/typings'
+import { ERROR_GENERAL_SUGGESTION } from 'utils/errors/suggestion'
+import { GeneralSuggestion } from 'database/models'
+interface generalSuggestionBody {
+    suggestion: string
+}
+export const generalSuggestion: Action<any, generalSuggestionBody, any , any> = async (req, res, next) => {
+    try {
+        const { suggestion } = await req.body
+        const sugg = await GeneralSuggestion.create({
+            'suggestion_text': suggestion
+        })
+        
+        return res.status(201)
+    } catch (e) {
+        await logError(`Error during adding suggstion`, e)
+        return next(ERROR_GENERAL_SUGGESTION)
+    }
+}
