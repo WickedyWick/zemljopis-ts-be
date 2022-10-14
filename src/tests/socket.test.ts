d89f72d2c68a1db09f48736ba70668aee6a713b7
WickedyWick <crvenialeksa@gmail.com>
2022-10-14 21:37:10 +0200

    .env changed and joinRoom tests finished

--- src/tests/socket.test.ts
+++ src/tests/socket.test.ts
@@ -17,14 +17,21 @@ interface JoinRoomInterface {
     ready: string,
     players: string[]
 }
+
 interface CreateRoomInterface { 
-    roomCode?: string,
-    sessionToken?: string
+    roomCode: string,
+    sessionToken: string
 }
+
+interface RegUserInterface {
+    sessionToken: string
+}
+
 const port = process.env.PORT || 8000
+const baseUrl = process.env.BASE_URL || 'http://localhost'
 const initSocket = () => {
     return new Promise((resolve, reject) => {
-        const socket = io(`http://localhost:${port}`, {
+        const socket = io(`${baseUrl}:${port}`, {
             'reconnection delay': 0,
             'reopen delay': 0,
             'force new connection': true
@@ -65,7 +72,7 @@ describe('Sockets', () => {
         const username = `AleksaTest`
         const response = await axios({
             method: 'post',
-            url: 'http://localhost:8000/home/createGame',
+            url: `${baseUrl}:${port}/home/createGame`,
             data: {
                 username,
                 roundTimeLimit,
@@ -107,4 +114,109 @@ describe('Sockets', () => {
         expect(serverRes[3]).toBe(`${roundTimeLimit}`)
         expect(serverRes[4]).toBe('0')
     })
+
+    it.each([
+        { roundTimeLimit: '60', playerCount: '2' },
+        { roundTimeLimit: '90', playerCount: '2' },
+        { roundTimeLimit: '120', playerCount: '2' },
+        { roundTimeLimit: '180', playerCount: '2' }
+    ])
+    ('should join room if multiple users', async({ roundTimeLimit, playerCount }) => {
+
+        //@ts-ignore
+        const clientSocket: Socket = await initSocket()
+        const username = `AleksaTest`
+        const createRoomApiResponse = await axios({
+            method: 'post',
+            url: `${baseUrl}:${port}/home/createGame`,
+            data: {
+                username,
+                roundTimeLimit,
+                playerCount
+            },
+            timeout: 20000
+        })
+
+        const createRoomData: CreateRoomInterface = await createRoomApiResponse.data
+
+        const serverResponse: Promise<JoinRoomInterface> | Error = new Promise((resolve, reject) => {
+            clientSocket.on(EVENTS.JOIN_ROOM, (response: JoinRoomInterface) => {
+                const _response = response
+                destroySocket(clientSocket)
+                resolve(_response)
+            })
+
+            setTimeout(() => {
+                reject(new Error('Failed to get response, connection timed out...'))
+            }, 5000)
+        })
+        
+        clientSocket.emit(EVENTS.JOIN_ROOM, ({
+            username,
+            roomCode: createRoomData.roomCode,
+            sessionToken: createRoomData.sessionToken 
+        }))
+
+        const serverResData: JoinRoomInterface = await serverResponse
+
+        expect(serverResData.CODE).toBe(200)
+        expect(serverResData.points).toBe('0')
+        expect(serverResData.ready).toBe(null)
+        expect(serverResData.players[0]).toBe(username)
+        expect(serverResData.players).toHaveLength(1)
+        expect(serverResData[0]).toBe('0')
+        expect(serverResData[1]).toBe(`${playerCount}`)
+        expect(serverResData[2]).toBe('0')
+        expect(serverResData[3]).toBe(`${roundTimeLimit}`)
+        expect(serverResData[4]).toBe('0')
+
+        //@ts-ignore
+        const clientSocket2: Socket = await initSocket()
+
+        const username2 = 'AleksaTest1'
+        const regUserApiResponse = await axios({
+            method: 'post',
+            url: `${baseUrl}:${port}/home/regUser`,
+            data: {
+                username: username2,
+                roomCode: createRoomData.roomCode
+            },
+            timeout: 20000
+        })
+
+        const regUserData: RegUserInterface = await regUserApiResponse.data
+
+        const serverResponse2: Promise<JoinRoomInterface> | Error = new Promise((resolve, reject) => {
+            clientSocket2.on(EVENTS.JOIN_ROOM, (response: JoinRoomInterface) => {
+                const _response = response
+                destroySocket(clientSocket2)
+                resolve(_response)
+
+                setTimeout(() => {
+                    reject(new Error('Failed to get response, connection timed out...'))
+                }, 5000)
+            })
+        })
+
+        clientSocket2.emit(EVENTS.JOIN_ROOM, ({
+            username: username2,
+            roomCode: createRoomData.roomCode,
+            sessionToken: regUserData.sessionToken
+        }))
+
+        const serverResData2: JoinRoomInterface = await serverResponse2
+
+        expect(serverResData2.CODE).toBe(200)
+        expect(serverResData2.points).toBe('0')
+        expect(serverResData2.ready).toBe(null)
+        expect(serverResData2.players[0]).toBe(username)
+        expect(serverResData2.players[1]).toBe(username2)
+        expect(serverResData2.players).toHaveLength(2)
+        expect(serverResData2[0]).toBe('0')
+        expect(serverResData2[1]).toBe(`${playerCount}`)
+        expect(serverResData2[2]).toBe('0')
+        expect(serverResData2[3]).toBe(`${roundTimeLimit}`)
+        expect(serverResData2[4]).toBe('0')
+    })
+
 })
